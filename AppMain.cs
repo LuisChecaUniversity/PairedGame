994941a1f849e75de6aaad346deb963bded35b77
Luis Checa <c008101c@student.staffs.ac.uk>
2014-11-26 11:40:36 +0000

    [+] Update initialisers for UI and GameEngine [LC]

--- AppMain.cs
+++ AppMain.cs
@@ -12,37 +12,29 @@ namespace PairedGame
 	{
 		private static GraphicsContext graphics;
 		
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-			Initialize ();
+			Initialize();
 
 			while (true) {
-				SystemEvents.CheckEvents ();
-				Update ();
-				Render ();
+				SystemEvents.CheckEvents();	// We check system events (such as pressing PS button, pressing power button to sleep, major and unknown crash!!)				
+				Director.Instance.Update();
+				UISystem.Update(Touch.GetData(0)); // Update UI Manager
+				Director.Instance.Render();
+				UISystem.Render(); // Render UI Manager
+				Director.Instance.GL.Context.SwapBuffers(); // Swap between back and front buffer
+				Director.Instance.PostSwap(); // Must be called after swap buffers - not 100% sure, imagine it resets back buffer to black/white, unallocates tied resources for next swap
 			}
+			Director.Terminate();	// Kill (terminate) the director, hence ending 2D scene program, once we are done with the scene (clicking red X button)
+			Sce.PlayStation.HighLevel.UI.UISystem.Terminate();
 		}
 
-		public static void Initialize ()
+		public static void Initialize()
 		{
-			// Set up the graphics system
-			graphics = new GraphicsContext ();
-		}
-
-		public static void Update ()
-		{
-			// Query gamepad for current state
-			var gamePadData = GamePad.GetData (0);
-		}
-
-		public static void Render ()
-		{
-			// Clear the screen
-			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-			graphics.Clear ();
-
-			// Present the screen
-			graphics.SwapBuffers ();
+			Director.Initialize(); // Initialises the GameEngine2D supplied by Sony.			
+			UISystem.Initialize(Director.Instance.GL.Context);
+			UISystem.SetScene(new Sce.PlayStation.HighLevel.UI.Scene(), null);			
+			Director.Instance.RunWithScene(new Sce.PlayStation.HighLevel.GameEngine2D.Scene(), true);	// Tell the Director to run the scene
 		}
 	}
 }
