09bac4982b80a4b2c4b3395c4f4a49cfa1e76d1e
Luis Checa <c008101c@student.staffs.ac.uk>
2014-12-03 10:11:15 +0000

    [+] Tile settings, loader, textures [LC]

--- AppMain.cs
+++ AppMain.cs
@@ -1,22 +1,23 @@
 using System;
-using System.Collections.Generic;
 
 using Sce.PlayStation.Core;
 using Sce.PlayStation.Core.Environment;
 using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.HighLevel.UI;
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 
 namespace PairedGame
 {
 	public class AppMain
-	{
-		private static GraphicsContext graphics;
-		
+	{		
 		public static void Main(string[] args)
 		{
 			Initialize();
-
-			while (true) {
+			bool quitGame = false;
+			
+			while (!quitGame) {
 				SystemEvents.CheckEvents();	// We check system events (such as pressing PS button, pressing power button to sleep, major and unknown crash!!)				
 				Director.Instance.Update();
 				UISystem.Update(Touch.GetData(0)); // Update UI Manager
@@ -31,10 +32,18 @@ namespace PairedGame
 
 		public static void Initialize()
 		{
-			Director.Initialize(); // Initialises the GameEngine2D supplied by Sony.			
+			// Initialises the GameEngine2D supplied by Sony.
+			Director.Initialize();
+			// Initialises the UI Framework supplied by Sony.
 			UISystem.Initialize(Director.Instance.GL.Context);
-			UISystem.SetScene(new Sce.PlayStation.HighLevel.UI.Scene(), null);			
-			Director.Instance.RunWithScene(new Sce.PlayStation.HighLevel.GameEngine2D.Scene(), true);	// Tell the Director to run the scene
+			// Load and store textures
+			TextureManager.AddAsset("tiles", new TextureInfo(new Texture2D("/Application/assets/dungeon_tiles.png", false),
+			                                                 new Vector2i(21, 5)));
+			TextureManager.AddAsset("chars", new TextureInfo(new Texture2D("/Application/assets/dungeon_objects.png", false),
+			                                                 new Vector2i(12, 1)));
+			// Tell the UISystem to run an empty scene
+			UISystem.SetScene(new Sce.PlayStation.HighLevel.UI.Scene(), null);
+			Director.Instance.RunWithScene(new CrawlerScreen(), true);
 		}
 	}
 }
--- Tile.cs
+++ Tile.cs
@@ -1,10 +1,12 @@
 using System;
 using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 
 namespace PairedGame
 {
-	public enum Collidables
+	public enum Collidable
 	{
 		None = 0,
 		Left,
@@ -19,25 +21,84 @@ namespace PairedGame
 	
 	public class Tile: SpriteTile
 	{
-		private bool isCollidable = false;
 		private bool isLootable = false;
 		private bool isOccupied = false;
-		private Collidables colliadableSides = 0;
+		private Collidable collidableSides = Collidable.None;
 		
 		public Tile(char loadKey, Vector2 position): base()
 		{
+			TextureInfo = TextureManager.Get("tiles");
 			Position = position;
+			Quad.S = TextureInfo.TileSizeInPixelsf;
 			
 			switch(loadKey)
 			{
-			case 'S':
-				TileIndex1D = 0;
+			case 'S': 
+				isOccupied = true;
+				TileIndex2D = new Vector2i(GameInfo.Rnd.Next(1, 4), GameInfo.Rnd.Next(1, 4));
 				break;
 			case 'X':
-				isCollidable = true;
+				TileIndex2D = new Vector2i(GameInfo.Rnd.Next(1, 4), GameInfo.Rnd.Next(1, 4));
+				break;
+			case 'D':
+				TileIndex2D = new Vector2i(GameInfo.Rnd.Next(8, 11), 1);
+				break;
+			case 'P':
+				TileIndex2D = new Vector2i(0, 4);
+				collidableSides = Collidable.TopLeft;
+				break;
+			case 'O':
+				TileIndex2D = new Vector2i(4, 4);
+				collidableSides = Collidable.TopRight;
+				break;
+			case 'I':
+				TileIndex2D = new Vector2i(0, 0);
+				collidableSides = Collidable.BottomLeft;
+				break;
+			case 'U':
+				TileIndex2D = new Vector2i(4, 0);
+				collidableSides = Collidable.BottomRight;
+				break;
+			case 'M':
+				TileIndex2D = new Vector2i(GameInfo.Rnd.Next(1, 4), 4);
+				collidableSides = Collidable.Top;
+				break;
+			case 'N':
+				TileIndex2D = new Vector2i(GameInfo.Rnd.Next(1, 4), 0);
+				collidableSides = Collidable.Bottom;
+				break;
+			case 'B':
+				TileIndex2D = new Vector2i(0, GameInfo.Rnd.Next(1, 4));
+				collidableSides = Collidable.Left;
+				break;
+			case 'V':
+				TileIndex2D = new Vector2i(4, GameInfo.Rnd.Next(1, 4));
+				collidableSides = Collidable.Right;
 				break;
 			default:
 				break;
+			}
+		}
+		
+		public static void Loader(string filepath, Scene scene)
+		{
+			int x = 0;
+			int y = 0;
+			int SIZE = 16;
+			Tile t;
+			foreach (string line in System.IO.File.ReadLines(filepath))
+			{
+				x = 0;
+			    foreach (char c in line.ToUpper())
+				{
+					if (c != ' ')
+					{
+						t = new Tile(c, new Vector2(x, y));
+						scene.AddChild(t);
+					}
+					x += SIZE;
+				}
+				y += SIZE;
 			}
 		}
 	}
