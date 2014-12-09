08dcdd5e52f37f02e9d503a92f7e17cad17f9dc4
JohnOswin94 <o018313c@student.staffs.ac.uk>
2014-12-09 20:46:49 +0000

    [+] Audio Manager [JO[

---
+++ AudioManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core.Audio;
+
+namespace PairedGame
+{
+	public static class AudioManager
+	{
+		public AudioManager ()
+		{
+		}
+	}
+}
+
