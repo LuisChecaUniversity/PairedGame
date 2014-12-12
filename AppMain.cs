5fd7430e8226e714cade68dbc930975fd9ab6219
Luis Checa <c008101c@student.staffs.ac.uk>
2014-12-12 08:41:59 +0000

    [+] Changed dungeon objects, added animation [LC]

--- AppMain.cs
+++ AppMain.cs
@@ -40,7 +40,7 @@ namespace PairedGame
 			TextureManager.AddAsset("tiles", new TextureInfo(new Texture2D("/Application/assets/dungeon_tiles.png", false),
 			                                                 new Vector2i(21, 5)));
 			TextureManager.AddAsset("entities", new TextureInfo(new Texture2D("/Application/assets/dungeon_objects.png", false),
-			                                                 new Vector2i(12, 1)));
+			                                                 new Vector2i(9, 13)));
 			// Tell the UISystem to run an empty scene
 			UISystem.SetScene(new Sce.PlayStation.HighLevel.UI.Scene(), null);
 			Director.Instance.RunWithScene(new CrawlerScreen(), true);
