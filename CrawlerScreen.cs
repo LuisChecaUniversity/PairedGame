8c2d475f203cdb433edd60feb380b46e4b238568
Luis Checa <c008101c@student.staffs.ac.uk>
2014-12-13 15:13:12 +0000

    [+] Zoom camera if InBattle [LC]

--- CrawlerScreen.cs
+++ CrawlerScreen.cs
@@ -17,21 +17,26 @@ namespace PairedGame
 		private void LoadLevel(int level)
 		{
 			// Clear scene
-			if (Children.Count > 0) Children.Clear();
+			if(Children.Count > 0)
+				Children.Clear();
 			// (Re)Initialize variables
 			Info.CameraHeight = 200f;
 			Info.LevelClear = false;
+			Info.InBattle = false;
 			Vector2 cameraCentre = new Vector2();
 			// Load level layout from file
 			Tile.Loader("/Application/assets/level" + level.ToString() + ".txt", ref cameraCentre, this);
 			// Reset camera to player position
 			Camera2D.SetViewFromHeightAndCenter(Info.CameraHeight, cameraCentre);
-			if (level != Info.LevelNumber) Info.LevelNumber = level;
+			if(level != Info.LevelNumber)
+				Info.LevelNumber = level;
 		}
 		
 		override public void Update(float dt)
 		{
-			if (Info.LevelClear && Info.LevelNumber < Info.MaxLevels)
+			Info.CameraHeight = Info.InBattle ? 150f : 200f;
+			
+			if(Info.LevelClear && Info.LevelNumber < Info.MaxLevels)
 			{
 				Info.LevelNumber++;
 				LoadLevel(Info.LevelNumber);
