816720813c89c38276851d0d9e33833329865d7a
Luis Checa <c008101c@student.staffs.ac.uk>
2014-12-13 15:13:41 +0000

    [+] Remove dead entities from scene [LC]

--- Entity.cs
+++ Entity.cs
@@ -5,10 +5,9 @@ using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 
 namespace PairedGame
 {
-	
 	public class Entity: SpriteTile
 	{
-		protected bool isAlive = true;
+		public bool IsAlive { get; protected set; }
 		
 		public Entity(Vector2 position):
 			base()
@@ -17,22 +16,9 @@ namespace PairedGame
 			// Size and position
 			Position = position;
 			Quad.S = TextureInfo.TileSizeInPixelsf;
+			IsAlive = true;
 			// Attach update function to scheduler
 			ScheduleUpdate();
-		}
-		
-		public Entity(int tileIndexY, Vector2i tileRangeX, Vector2 position, float interval=0.2f):
-			this(position)
-		{
-			// Create animation function. tileRange = { xMinTile2D, xMaxTile2D }
-			TileIndex2D = new Vector2i(tileRangeX.X, tileIndexY);
-			ScheduleInterval( (dt) => {
-				if(IsAlive)
-				{
-					int tileIndex = TileIndex2D.X < tileRangeX.Y ? TileIndex2D.X + 1 : tileRangeX.X;
-					TileIndex2D.X = tileIndex;
-				}
-			}, interval);
 		}
 		
 		public Entity(int tileIndex, Vector2 position):
@@ -44,8 +30,9 @@ namespace PairedGame
 		override public void Update(float dt)
 		{
 			base.Update(dt);
+			if(!IsAlive)
+				Parent.RemoveChild(this, true);
 		}
 		
-		public bool IsAlive { get{ return isAlive; } set{ isAlive = value; } }
 	}
 }
